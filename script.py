b3513d42c3b1e21569daba88faaa8e03aa7e46cc
Neology92 <oskarlegner@gmail.com>
2019-12-03 05:54:32 +0100

    Create canvas and first slider

--- script.py
+++ script.py
@@ -1,55 +1,36 @@
 from tkinter import *
 
-# key down func
+# functions
 
 
-def click():
-    entered_text = textentry.get()
-    output.delete(0.0, END)
-    try:
-        answer = my_dictionary[entered_text]
-    except:
-        answer = "I don't get it"
-    output.insert(END, answer)
+def slider():
+    selection = "Set = " + str(var.get())
+    value.config(text=selection)
 
 
 # main
 window = Tk()
+window.minsize(450, 600)
 window.title("Hello")
 window.configure(background="black")
 
-# Photo
-photo1 = PhotoImage(file="img.png")
-Label(window, image=photo1, bg="black").grid(row=0, column=0, sticky=W)
-Label(window, image=photo1, bg="black").grid(row=0, column=1, sticky=W)
 
-# Label
-Label(window, text="Write some text", bg="black", fg="white",
-      font="none 12 bold").grid(row=1, column=0, sticky=E)
+var = DoubleVar()
+scale = Scale(window, variable=var)
+scale.place(x=25, y=10)
 
-# text entry
-textentry = Entry(window, width=20, bg="white")
-textentry.grid(row=2, column=0, sticky=E)
+button = Button(window, text="Apply", command=slider)
+button.place(x=25, y=120)
 
-# submit button
-submitbutton = Button(window, text="submit", width=10, command=click)
-submitbutton.grid(row=2, column=1, sticky=W)
+value = Label(window)
+value.place(x=25, y=150)
 
-# Label 2
-Label(window, text="Output:", bg="black", fg="white",
-      font="none, 16 bold").grid(row=1, column=1, sticky=W)
+canvas = Canvas(window, width=400, height=400)
+canvas.place(x=25, y=180)
+canvas.create_rectangle((0, 0, 400, 400), fill="#ffffff",
+                        width=0)  # rysuję biały prostokąt
 
-# Output
-output = Text(window, width=30, height=1, wrap=WORD, background="white")
-output.grid(row=1, column=1, sticky=E)
-
-# dictionary
-my_dictionary = {
-    'Hi': 'Hello',
-    'hi': 'Hello',
-    'Good bye': 'See Ya',
-    'good bye': 'See Ya',
-}
+# arc_id = canvas.create_arc((0, 0, 50, 25), start=20, extend=30, style=ARC)
 
 
 # run main loop
