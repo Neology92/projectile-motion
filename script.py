cc67af0909d774be7c4906cce43cbbe479d67778
Neology92 <oskarlegner@gmail.com>
2019-12-04 23:15:32 +0100

    Add equations based on time

--- script.py
+++ script.py
@@ -1,37 +1,43 @@
-from tkinter import *
+from numpy import *
+from matplotlib.pyplot import *
+import math
 
-# functions
+Vo = 300         # m/s
+angle = 45       # degree
+g = 10          # m/s^2
+res = 0.24      # kg/m
+# res = 0      # kg/m
 
+def degree_to_radian(angle):
+    return  angle / 180 * math.pi
 
-def slider():
-    selection = "Set = " + str(var.get())
-    value.config(text=selection)
-
-
-# main
-window = Tk()
-window.minsize(450, 600)
-window.title("Hello")
-window.configure(background="black")
-
-
-var = DoubleVar()
-scale = Scale(window, variable=var)
-scale.place(x=25, y=10)
-
-button = Button(window, text="Apply", command=slider)
-button.place(x=25, y=120)
-
-value = Label(window)
-value.place(x=25, y=150)
-
-canvas = Canvas(window, width=400, height=400)
-canvas.place(x=25, y=180)
-canvas.create_rectangle((0, 0, 400, 400), fill="#ffffff",
-                        width=0)  # rysuję biały prostokąt
-
-# arc_id = canvas.create_arc((0, 0, 50, 25), start=20, extend=30, style=ARC)
+rad = degree_to_radian(angle)
 
 
-# run main loop
-window.mainloop()
+def foo_x(t):   
+    return Vo * math.cos(rad) / res * (1 - math.e**(-res  * t))
+
+def foo_y(t):
+    return (( Vo * math.sin(rad) + g) * (1 - math.e**(-res  * t)) - g*t)/res
+
+def time_to_back_on_earth():
+    return 2*Vo * math.sin(rad)/g
+
+def throw_range():
+    return foo_x(time_to_back_on_earth())
+
+
+t = linspace(0, time_to_back_on_earth(), 10000)    # 51 points between 0 and 3
+
+y = zeros(len(t))         # allocate y with float elements
+x = zeros(len(t))         # allocate y with float elements
+
+for i in range(0,len(t)):
+    y[i] = foo_y(t[i])
+    x[i] = foo_x(t[i])
+
+
+ylim(0,1.5*throw_range())
+xlim(0,1.5*throw_range())
+plot(x, y)
+show()
