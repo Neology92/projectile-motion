4bad7d05836f04335538f4d31500e45d3cfb8b6d
Neology92 <oskarlegner@gmail.com>
2019-12-05 00:35:32 +0100

    Not working, but just y equation

--- script.py
+++ script.py
@@ -1,12 +1,11 @@
 from numpy import *
 from matplotlib.pyplot import *
-import math
+from math import *
 
-Vo = 20         # m/s
-angle = 89.99       # degree
-g = 10          # m/s^2
-res = 0.24      # kg/m
-# res = 0      # kg/m
+v = 20         
+angle = 45       
+g = 10          
+k = 0.4      
 
 def degree_to_radian(angle):
     return  angle / 180 * math.pi
@@ -16,22 +15,27 @@ rad = degree_to_radian(angle)
 
 
 def foo_y(x):
-    return (-1/2) * (g / (Vo**2 * math.cos(rad)**2) ) * x**2 + math.tan(rad) * x
+    return 1/k * (v*sin(rad) + g*log(-x*k / (v*cos(rad) ) + 1)) + 1/(k**2) * (g - (g + k*v*sin(rad)) * (-x*k / (v*cos(rad) ) + 1))
 
-def throw_range():
-    return Vo**2 * math.sin(2*rad) / g
+def approximate_throw_range():
+    return v**2 * math.sin(2*rad) / g
 
 
 
-x = linspace(0, throw_range(), 10000)    # 51 points between 0 and 3
+x = linspace(0, approximate_throw_range(), 10000)    # 51 points between 0 and 3
 
 y = zeros(len(x))         # allocate y with float elements
 
 for i in range(0,len(x)):
     y[i] = foo_y(x[i])
 
+# print(x)
+# print(y)
 
-ylim(0, ( 1.5*throw_range() if angle < 80  else 1.1*Vo ) )
-xlim(0, ( 1.5*throw_range() if angle < 80  else 1.1*Vo ) )
+
+# ylim(0, ( 1.5*approximate_throw_range() if angle < 80  else 1.1*v ) )
+ylim(0, 100 )
+# xlim(0, ( 1.5*approximate_throw_range() if angle < 80  else 1.1*v ) )
+xlim(0, 100 )
 plot(x, y)
 show()
