b317b9adb0649b062d3031f616443c6821020b68
Neology92 <oskarlegner@gmail.com>
2019-12-12 01:08:38 +0100

    MVP

--- script.py
+++ script.py
@@ -2,44 +2,90 @@ from numpy import *
 from matplotlib.pyplot import *
 from math import *
 
-v = 300         # m/s
+v = 10         # m/s
 angle = 45       # degree
 g = 10          # m/s^2
-k = -0.24      # kg/m
+k = 0.24      # kg/m
 
 def degree_to_radian(angle):
-    # return  angle / 180 * math.pi
-    return  angle 
+    return  angle / 180 * pi
+
+def _input(entry, type_of):
+    if type_of == 'angle':
+        angle = float(input(entry))
+        while (angle < 0 or angle > 90):
+            print("Angle should be between 0 and 90")
+            angle = float(input(entry))
+        return angle
+    elif type_of == 'velocity':
+        v = float(input(entry))
+        while (v <= 0 ):
+            print("Velocity should be higher than 0")
+            v = float(input(entry))
+        return v
+    elif type_of == 'gravity':
+        g = float(input(entry))
+        while (g <= 0 ):
+            print("Gravity acceleration should be higher than 0")
+            g = float(input(entry))
+        return g
+    elif type_of == 'air_resistance':
+        k = float(input(entry))
+        while (k < 0 ):
+            print("Air resistance const cannot be less than 0")
+            k = float(input(entry))
+        return k
+        
+        
+
+
+    return float(input(entry))
+
+###############################
+v = _input("Initial velocity: ", "velocity")
+angle = _input("Throw angle: ", "angle")
+g = _input("Earthly attraction: ", "gravity")
+k = _input("Air resistance const: ", "air_resistance")
+###############################
+
 
 rad = degree_to_radian(angle)
+vx = v * cos(rad)
+vy = v * sin(rad)
+
+dt = 0.01         #s
+t = 0
+
+x = [0]
+y = [0]
+
+stop = False
+while (not stop):
+    t += dt
+
+    next_x = x[-1] + vx*dt
+    next_y = y[-1] + vy*dt
+
+    # next_x = vx*t
+    # next_y = vy*t - (g*t**2)/2
+
+    y.append(next_y)
+    x.append(next_x)
+
+    if(y[-1] <= 0):
+        stop = True
+    else:
+        vx = vx - k*vx*dt
+        vy = vy - g*dt - k*vy*dt
 
 
-def foo_x(t):   
-    # return Vo * math.cos(rad) / res * (1 - math.e**(-res  * t))
-    return (v*cos(rad) / k) * ( 1 - e**(-k*t) ) 
+# def range_should_be(vo,)
 
-def foo_y(t):
-    # return (( Vo * math.sin(rad) + g) * (1 - math.e**(-res  * t)) - g*t)/res
-    return 1/k + (v*sin(rad) - g*t) + (1/k**2) * (g - e**(-k*t) * ( g + k*v*sin(rad) ))
+max_y = max(y)
+max_x = max(x)
+lim = 1.1 * max(max_x, max_y)
 
-def time_to_back_on_earth():
-    return 2*v * math.sin(rad)/g
-
-def throw_range():
-    return foo_x(time_to_back_on_earth())
-
-
-t = linspace(0, 100, 201)    
-
-y = zeros(len(t))         # allocate y with float elements
-x = zeros(len(t))         # allocate y with float elements
-
-for i in range(0,len(t)):
-    y[i] = foo_y(t[i])
-    x[i] = foo_x(t[i])
-
-
-# ylim(0,1.5*throw_range())
-# xlim(0,1.5*throw_range())
+ylim(0,lim)
+xlim(-0.01*lim,lim)
 plot(x, y)
 show()
